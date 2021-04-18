faf703d11543d0e686a28610136dad3d9535f9e2
SeongBeen Jeon <curiousbeeny@gmail.com>
2021-04-19 01:36:08 +0900

    add Error handle middleware using njk

---
+++ template/error_app.js
@@ -0,0 +1,16 @@
+//...
+
+app.use((req,res,next)=>{
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+})
+
+app.use((err,req,res,next)=>{
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.stauts(err.status || 500);
+    res.rend('./error');
+})
+
+//...
